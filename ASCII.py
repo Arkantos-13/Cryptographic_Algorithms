0c913de6f71b7a706350913d6bdfca88faec6770
GeoP <64365608+Arkantos-13@users.noreply.github.com>
2021-05-31 19:22:55 +0300

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -1,3 +1,8 @@
+"""
+Convert a normal text to an ASCII code
+"""
+
+
 def txt_to_ascii():
     message = input('Please type the text you would like to convert: ')
 
@@ -6,31 +11,43 @@ def txt_to_ascii():
     print('Your new text in ASCII code is ready:', convert_to_ascii)
 
 
-def ascii_to_txt():
-    ## number of elements
+"""
+Convert an ASCII code text, back to normal text with lowercase and uppercase letters
+"""
 
+
+def ascii_to_txt():
+
+    # number of elements
     numbers_elements = int(input("Enter number of elements : "))
 
-    # Below line read inputs from user using map() function
+    # This line read the text using the map() function
 
     elemnets = list(map(int, input("\nEnter the numbers : ").strip().split()))[:numbers_elements]
 
-    convert_to_text = ''.join([chr(i) for i in elemnets])
+    # We join each letter back together
+    convert_to_text = ''.join(chr(i) for i in elemnets)
     print(convert_to_text)
 
+    
+    
+"""
+We give the user only two options in this cipher
+"""
 
-mode = int(input(
 
-    'Press 1. If you want to convert your text in ASCII Code \n'
-    'Press 2. If you want to convert your ASCII Code to a text \n'
-))
+def ASCII():
 
+    mode = int(input(
 
-def ASCII(mode):
+        'Press 1. If you want to convert your text into ASCII code: \n'
+        'Press 2. If you want to convert your ASCII code to a text: \n'
+    ))
+
     if mode == 1:
         txt_to_ascii()
     else:
         ascii_to_txt()
 
 
-ASCII(mode)
+ASCII()
