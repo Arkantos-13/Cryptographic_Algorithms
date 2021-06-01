32c6828f0477b7d5442d8f0b0d089d8d74fa2384
GeoP <64365608+Arkantos-13@users.noreply.github.com>
2021-06-01 11:59:09 +0300

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -1,26 +1,47 @@
-def text_to_binary(msg):
-    binary = ' '.join(format(ord(x), 'b') for x in msg )
-    print(binary)
+"""
+Convert a message to binary
+"""
 
 
-def binary_to_text(msg):
-    text = ''.join([chr(int(s, 2)) for s in msg.split()])
-    print(text)
-
-msg = input('Type the message you want to convert: ')
-ch = int(input('Please choose \n'
-               '1. To convert a text to a binary code: \n' 
-               '2. To convert a binary code to a text: \n'
-               ))
+def text_to_binary(message):
+    binary = ' '.join(format(ord(x), 'b') for x in message)
+    print('Your converted message is the following: \n', binary)
 
 
-def main(msg, ch):
-    if ch == 1:
-        text_to_binary(msg)
-    elif ch == 2:
-        binary_to_text(msg)
+"""
+Convert a binary message back to a normal message
+"""
+
+
+def binary_to_text(message):
+    new_message = ''.join([chr(int(s, 2)) for s in message.split()])
+    print('Your converted binary code is the following: \n', new_message)
+
+
+"""
+We ask the user to type his choice.
+One choice is to convert a message to binary or vice versa.
+After that, we ask for his message
+"""
+
+choice_of_user = int(input('Please choose \n'
+                           '1.To convert a text to a binary code: \n'
+                           '2.To convert a binary code to a text: \n'
+                           ))
+
+message = input('Type the message you want to convert: ')
+
+
+def Binary(message, choice_of_user):
+
+    if choice_of_user == 1:
+        text_to_binary(message)
+
+    elif choice_of_user == 2:
+        binary_to_text(message)
+
     else:
-        print('Wrong choice!')
+        print('Wrong choice! Please try again')
 
 
-main(msg,ch)
+Binary(message, choice_of_user)
