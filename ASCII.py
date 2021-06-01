3bcf76d718f33ce7ebc4d8655cea451733af96c1
GeoP <64365608+Arkantos-13@users.noreply.github.com>
2021-06-01 11:30:32 +0300

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -1,7 +1,5 @@
 """
 Convert a normal text to an ASCII code
-""""""
-Convert a normal text to an ASCII code
 """
 
 
@@ -32,57 +30,6 @@ def ascii_to_txt():
     print(convert_to_text)
 
 
-"""
-We give the user only two options in this cipher
-"""
-
-
-def ASCII():
-
-    mode = int(input(
-
-        'Press 1. If you want to convert your text into ASCII code: \n'
-        'Press 2. If you want to convert your ASCII code to a text: \n'
-    ))
-
-    if mode == 1:
-        txt_to_ascii()
-    else:
-        ascii_to_txt()
-
-
-ASCII()
-
-
-
-def txt_to_ascii():
-    message = input('Please type the text you would like to convert: ')
-
-    convert_to_ascii = [ord(c) for c in message]
-
-    print('Your new text in ASCII code is ready:', convert_to_ascii)
-
-
-"""
-Convert an ASCII code text, back to normal text with lowercase and uppercase letters
-"""
-
-
-def ascii_to_txt():
-
-    # number of elements
-    numbers_elements = int(input("Enter number of elements : "))
-
-    # This line read the text using the map() function
-
-    elemnets = list(map(int, input("\nEnter the numbers : ").strip().split()))[:numbers_elements]
-
-    # We join each letter back together
-    convert_to_text = ''.join(chr(i) for i in elemnets)
-    print(convert_to_text)
-
-    
-    
 """
 We give the user only two options in this cipher
 """
