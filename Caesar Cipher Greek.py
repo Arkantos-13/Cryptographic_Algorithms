da9a3b51749783b39211352caa4bf195e090ec14
GeoP <64365608+Arkantos-13@users.noreply.github.com>
2021-06-01 13:07:58 +0300

    Update Caesar Cipher Greek.py

--- Caesar Cipher Greek.py
+++ Caesar Cipher Greek.py
@@ -1,13 +1,22 @@
 """
+This code works as the original Caesar Cipher and use only the uppercase letters.
+In other words it changes only the letters of a text and anything else.
+If you want to change the numbers and the punctuations of a plain text
+you can view my other Caesar Ciphers that i have uploaded.
+
 Here is the greek alphabet.
-The code should work for every language if you change the alphabet
+The code should work for every language if you change the alphabet.
 """
+
 alphabets = 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ'
 
 """
-First we will make the encryption version of the cipher
+Here you can view the Encryption algorithm of the Caesar Cipher
 """
+
 def encryption(plain_text,shift):
+
+    # Here we create the new shifted alphabet that we will use
     shifted_alphabet = alphabets[shift:] + alphabets[:shift]
     cipher_text = " "
 
@@ -15,18 +24,23 @@ def encryption(plain_text,shift):
         char = plain_text[letter]
         position = alphabets.find(char.upper())
 
-        # It creates the spaces between the words in the plaintext
+        # It creates the spaces between the words from the plaintext
         if position == -1:
             cipher_text += char
+
+        # It changes the letters from the new shifted_alphabet
         else:
             cipher_text += shifted_alphabet[position]
 
     return cipher_text
 
 """
-Now, we will make the decryption version of our cipher
+Here you can view the Decryption algorithm of the Caesar Cipher
 """
+
 def decryption(plain_text, shift):
+
+    # Here we create the new shifted alphabet that we will use
     shifted_alphabet = alphabets[-shift:] + alphabets[:-shift]
     cipher_text = " "
 
@@ -35,46 +49,61 @@ def decryption(plain_text, shift):
         char = plain_text[letter]
         position = alphabets.find(char.upper())
 
-        # It creates the spaces between the words in the plaintext
+        # It creates the spaces between the words from the plaintext
         if position == -1:
             cipher_text += char
+
+        # It changes the letter from the new shifted_alphabet
         else:
             cipher_text += shifted_alphabet[position]
 
     return cipher_text
 
+"""
+The final code
+"""
 
 def Caesar():
-
-    choice_of_user = int(input('P1ease choose one of the following belong: \n Enter 1. '  
-                               'For Encryption:\n Enter 2. For Decryption: \n '
+    # The choices that we give to the user
+    choice_of_user = int(input('Please choose one of the following belongs: \n '
+                               'Enter 1. For Encryption:\n '
+                               'Enter 2. For Decryption: \n '
                                'Enter 3. For Brute Force Attack: \n '))
 
     if (choice_of_user == 1) | (choice_of_user == 2):
 
-        # Info of the User
+        # Information that the user have to type each time
         plain_text = input('Please type your text that you want to Encrypt/Decrypt: \n')
-        shift = int(input('Please type your key number (0-25) so to encrypt/decrypt the message: \n'))
+        shift = int(input('Please type your key number (0-23) so to encrypt/decrypt the message: \n'))
 
         if choice_of_user == 1:
-            print('You chose the number', choice_of_user, 'so you just want to encrypt your message:')
-            print('The encrypted message is: ', encryption(plain_text, shift))
+            print('You chose the number', choice_of_user, 'so you just want to encrypt your message:'
+                  'The encrypted message is: ', encryption(plain_text, shift))
 
-        elif choice_of_user == 2:
-            print('You chose the number', choice_of_user, 'so you just want to decrypt your message:')
-            print('The decrypted message is: ', decryption(plain_text, shift))
+        else:
+            print('You chose the number', choice_of_user, 'so you just want to decrypt your message:'
+                  'The decrypted message is: ', decryption(plain_text, shift))
 
     elif choice_of_user == 3:
 
-        # BRUTE FORCE ATTACK
+        """
+        BRUTE FORCE ATTACK ON THE CIPHER
+        
+        You can break any encrypted message without knowing the secret key
+        """
         message_to_break = input('Type your message you want to apply Brute Force Attack: ')
         print('Your original message is somewhere below: \n')
 
-        for i in range(26):
+        """
+        In other words here we try all the possible combination and print it out
+        The range depends from the length of the alphabet
+        """
+
+        for i in range(24):
             print(' shift{:2} : {}'.format(i, decryption(message_to_break, shift=i)))
 
     else:
-        print('\n', 'Please type valid input')
+        print('Please type a valid input and try again')
 
 
 Caesar()
