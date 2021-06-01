62d561f0ffefe5af51294cbe3a3342dbd4af02f3
GeoP <64365608+Arkantos-13@users.noreply.github.com>
2021-06-01 19:47:47 +0300

    Update Encryption of Picture.py

--- Encryption of Picture.py
+++ Encryption of Picture.py
@@ -1,8 +1,7 @@
-
-
 '''
 User's input:the path of image.
 '''
+
 img_path = input(r'Enter path of Image:')
 
 
@@ -10,6 +9,7 @@ img_path = input(r'Enter path of Image:')
 '''
 User's input:the key to encrypt or decrypt.
 '''
+
 key = int(input('Enter Key for encryption or decryption of Image:'))
 
 
@@ -17,6 +17,7 @@ key = int(input('Enter Key for encryption or decryption of Image:'))
 '''
 Open file to read data.
 '''
+
 f = open(img_path, 'rb')
 
 
@@ -24,14 +25,15 @@ f = open(img_path, 'rb')
 '''
 Storing image data in image variable
 '''
+
 image = f.read()
 f.close()
 
-#
 
 '''
 Converting image into byte array.
 '''
+
 image = bytearray(image)
 
 
@@ -39,6 +41,7 @@ image = bytearray(image)
 '''
 Perform XOR operation on each value of bytearray.
 '''
+
 for index, values in enumerate(image):
     image[index] = values ^ key
 
@@ -48,6 +51,7 @@ for index, values in enumerate(image):
 '''
 Open file to write data.
 '''
+
 f1 = open(img_path, 'wb')
 
 
@@ -55,6 +59,7 @@ f1 = open(img_path, 'wb')
 '''
 Write encrypted data of image.
 '''
+
 f1.write(image)
 f1.close()
 print('Encryption/Decryption Done...')
